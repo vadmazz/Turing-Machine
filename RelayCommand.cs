98a71c4ed30cb905f3a7d81b459a48a792ee1a86
vadmazz <vadmazz2013@yandex.ru>
2019-11-13 21:38:04 +0300

    InitCommit

---
+++ RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TuringMachine
+{
+    class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Func<object, bool> _canExecute;
+
+        private readonly Action<object> _onExecute;
+
+        //---Конструкторы
+        public RelayCommand(Action<object> execute)
+        {
+            _onExecute = execute;
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _onExecute = execute;
+        }        
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null || _canExecute(parameter))
+                return true;
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_onExecute != null)            
+                _onExecute(parameter);            
+        }
+    }
+}
